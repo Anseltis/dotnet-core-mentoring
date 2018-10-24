957a0c21f927e4a2379b25764f90e0f79f964e75
Yurij Rumyancev <yurij_rumyancev@epam.com>
2018-10-25 02:57:14 +0300

    Add the assembler class

---
+++ src/ProgramL01/ParseIntExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace ESystems.Mentoring.ProgramL01
+{
+    public static class ParseIntExtension
+    {
+        public static ParseIntSeed Concat(this ParseIntSeed left, ParseIntSeed right) 
+            => new ParseIntSeed(left.Value * right.Basis + right.Value, left.Basis * right.Basis);
+    }
+}
